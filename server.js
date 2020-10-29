f69c88ba50072457c232ed4d03bdc7e1743fabce
Bhavya Bhut <pbhavy4@gmail.com>
2020-10-29 11:35:02 +0530

    defined schema

---
+++ server.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config()
+const morgan = require("morgan");
+const cors = require("cors");
+
+const app = express()
+app.use(express.json());
+app.use(cors());
+
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan('tiny'));
+}
+
+// static;
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+
+    app.get("*", (req, res) =>
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    );
+}
+
+
+//database connection
+
+mongoose.connect(process.env.DB_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("database connect....");
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port,()=>{
+    console.log(`Server is running in port ${port}`);
+})
