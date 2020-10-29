aa6dac2b62abfa8193ef04826323c488dc14b890
Bhavya Bhut <pbhavy4@gmail.com>
2020-10-29 13:05:57 +0530

    done college api

---
+++ consts.js
@@ -0,0 +1,5 @@
+const errorMessage = {
+  NO_RESOURCE: "No resource found",
+};
+
+module.exports = errorMessage;
--- controller/college.js
+++ controller/college.js
@@ -1,21 +1,52 @@
-const College = require('../schemas/College')
+const College = require("../schemas/College");
+const { resourceError, serverError } = require("../helper/errorHandler");
+const { success } = require("../helper/successHandler");
+const { NO_RESOURCE } = require("../consts");
 
-exports.getColleges = (req,res,next)=>{
-    res.send('all clgs')
-
-}
-
-exports.getCollegeById = (req, res, next) => {
-  res.send(" clg by id");
+exports.getColleges = async (req, res, next) => {
+  try {
+    const colleges = await College.find();
+    success(res, colleges);
+  } catch (error) {
+    serverError(res, error);
+  }
 };
 
+exports.getCollegeById = async (req, res, next) => {
+  const { collegeId } = req.params;
+  console.log(collegeId);
+  if (!collegeId) resourceError(res, "Please enter college");
+  try {
+    const college = await College.findById(collegeId);
+    if (college) success(res, college);
+    else resourceError(res, NO_RESOURCE);
+  } catch (error) {
+    serverError(res, error);
+  }
+};
 
-exports.getCollegesByState = (req,res,next)=>{
-    res.send(' clgs by state')
+exports.getCollegesByState = async (req, res, next) => {
+  const { stateId } = req.params;
+  if (!stateId) resourceError(res, "Please enter state");
+  try {
+    const regex = new RegExp(stateId, "i");
+    const colleges = await College.find({ state: { $regex: regex } });
+    if (colleges) success(res, colleges);
+    else resourceError(res, NO_RESOURCE);
+  } catch (error) {
+    serverError(res, error);
+  }
+};
 
-}
-
-exports.getCollegesByLocation = (req,res,next)=>{
-    res.send(' clgs by location')
-
-}
+exports.getCollegesByLocation = async (req, res, next) => {
+  const { locationId } = req.params;
+  if (!locationId) resourceError(res, "Please enter city or location");
+  try {
+    const regex = new RegExp(locationId, "i");
+    const colleges = await College.find({ city: { $regex: regex } });
+    if (colleges) success(res, colleges);
+    else resourceError(res, NO_RESOURCE);
+  } catch (error) {
+    serverError(res, error);
+  }
+};
