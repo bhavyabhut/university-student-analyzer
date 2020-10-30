43b2ee13c80af3ec0703437e1f91ad87d82aedee
Bhavya Bhut <pbhavy4@gmail.com>
2020-10-30 18:06:01 +0530

    course chart client

--- client/src/api.js
+++ client/src/api.js
@@ -7,5 +7,6 @@ const API = {
   collegeByState: "/v1/college/state/:stateId",
   collegeByLocation: "/v1/college/location/:locationId",
   collegeChart: "/v1/college/chart/pie",
+  collegeCourseChart: "/v1/college/chart/pie/course",
 };
 export default API;
