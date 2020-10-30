a4d2396786b0181f28dd12058bd29c7d55bd48eb
Bhavya Bhut <pbhavy4@gmail.com>
2020-10-30 11:51:40 +0530

    change api

--- client/src/api.js
+++ client/src/api.js
@@ -1,11 +1,11 @@
 const API = {
-  students: "http://localhost:5000/v1/student/",
-  student: "http://localhost:5000/v1/student/:studentId",
-  colleges: "http://localhost:5000/v1/college/",
-  college: "http://localhost:5000/v1/college/:collegeId",
-  collegeStudent: "http://localhost:5000/v1/student/college/:collegeId",
-  collegeByState: "http://localhost:5000/v1/college/state/:stateId",
-  collegeByLocation: "http://localhost:5000/v1/college/location/:locationId",
-  collegeChart: "http://localhost:5000/v1/college/chart/pie",
+  students: "/v1/student/",
+  student: "/v1/student/:studentId",
+  colleges: "/v1/college/",
+  college: "/v1/college/:collegeId",
+  collegeStudent: "/v1/student/college/:collegeId",
+  collegeByState: "/v1/college/state/:stateId",
+  collegeByLocation: "/v1/college/location/:locationId",
+  collegeChart: "/v1/college/chart/pie",
 };
 export default API;
